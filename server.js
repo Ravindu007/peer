bc6bdc6b1ac8717c8d9ac99e7f040dd1bcbed341
Ravindu007 <ravindu0504@gmail.com>
2024-05-14 12:37:29 +0530

    added count

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const peerServer =  PeerServer({
     path: "/",
 });
 
+let peerCount = 0;
+
 // Start the PeerServer
 peerServer.on('connection', (client) => {
     console.log(`Client connected: ${client.getId()}`);
+    peerCount = peerCount + 1;
+    console.log("Total peers in the server: ", peerCount)
 });
 
 peerServer.on('disconnect', (client) => {
     console.log(`Client disconnected: ${client.getId()}`);
+    peerCount =- peerCount - 1 ;
+    console.log("Total peers in the server: ", peerCount)
 });
